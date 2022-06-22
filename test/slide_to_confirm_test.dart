bb65f798e257e73b2da38855669f334da401f38c
Asadeck <asad_3p@coinbeam.com>
2022-06-22 17:48:40 +0500

    just testing

--- test/slide_to_confirm_test.dart
+++ test/slide_to_confirm_test.dart
@@ -3,5 +3,10 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:slide_to_confirm/slide_to_confirm.dart';
 
 void main() {
-  
+  test('Given int and decimal places When converted Then returns type double',
+      () {
+    // Assert
+    expect(double, double,
+        reason: 'the method should return a value of type double');
+  });
 }
